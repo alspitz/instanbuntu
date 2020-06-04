842cddb2f947835f95c3751b3739aa37995c6718
Alex Spitzer <aes368@cornell.edu>
2020-06-04 14:50:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Instanbuntu
+
+[instanbuntu.sh](instanbuntu.sh) can be used to quickly (~10 - 15 minutes with a fast internet connection) install an Ubuntu system inside an existing Linux system.
+This is what I have used to run ROS inside a non Ubuntu machine for many years now and have had no issues.
+This is also useful if you want to run a newer or older Ubuntu version than your host computer.
+For example, if a server runs on Ubuntu 14.04, Ubuntu 16.04 can be setup in this way for continuous integration tests.
+Additionally, this allows you to have N different Ubuntu installations of various versions, each with an isolated filesystem.
+
+The only dependencies on the host system are the programs debootstrap and schroot.
+
+This is known to work with ROS Kinetic and Ubuntu Xenial (see below) but others flavors are supported by changing the appropriate names at the top of the script.
+While running, you will need to provide your password for sudo access.
+The script is designed so that it can be re-run upon failure, but some harmful edge cases may be possible.
+When in doubt, you can consult the script to continue manual installation upon a failure.
+If you do experience a failure, please feel free to file an issue or submit a pull request.
+
+# Tested Configurations
+
+| Ubuntu Flavor | ROS Flavor |
+| -----         | -----  |
+| Trusty | Indigo |
+| Xenial | Kinetic |
+| Zesty | Lunar |
+| Bionic | Melodic |
+
+# Known Limitations
+
+The chosen Ubuntu flavor must have packages for the chosen ROS flavor. e.g. Ubuntu Artful will not work with ROS Lunar, because Lunar is not officially supported on Artful and thus does not have the packages by default.
+A manual installation may still work however.
+
+Using an End of Life Ubuntu flavor, such as Zesty, requires changing the deboostrap URL from archive.ubuntu.com to old-releases.ubuntu.com.
+
+In order to run 3D accelerated programs such as rviz, additional graphics drivers such as nvidia-384 need to be installed manually.
+
+# Tips
+
+For easy opening of a terminal, set up a keyboard shortcut to directly open a terminal inside the chroot.
+For example, if using xterm, bind a keyboard shortcut to xterm -e "schroot -c xenial-insta" (change terminal program and chroot name appropriately).
---
+++ instanbuntu.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+###############################################################################
+# Usage:
+#   ./instanbuntu.sh will generate a folder in the CWD named ${flavor} that
+#   contains the Ubuntu system.
+#
+# Dependencies:
+# - debootstrap
+# - schroot
+# - internet connection
+# - a couple GB of disk space
+###############################################################################
+
+flavor=bionic
+ros_flavor=melodic
+arch=amd64
+
+set -e
+set -u
+set -o pipefail
+
+chroot_name="${flavor}-insta"
+
+chroot_dir="$(pwd)/${flavor}"
+
+if [ -e "${chroot_dir}" ]; then
+  echo "WARNING: \"${chroot_dir}\" already exists."
+  echo "	Press enter to assume the debootstrap succeeded and proceed without it."
+  echo "	The alternative is to"
+	echo "		exit with Control C,"
+	echo " 		delete \"${chroot_dir}\","
+	echo "		and restart."
+  read
+else
+  mkdir -p "${chroot_dir}"
+  sudo debootstrap --arch="${arch}" "${flavor}" "${flavor}"/ http://archive.ubuntu.com/ubuntu
+fi
+
+schroot_conf_entry="
+[${chroot_name}]
+description=${flavor} instanbuntu
+type=directory
+directory=${chroot_dir}
+users=${USER}
+root-users=${USER}
+root-groups=${USER}
+aliases=default"
+
+sources_add=" universe multiverse"
+
+bashrc_check="
+if [ \\\\\\\${SCHROOT_USER} ]; then
+  source /opt/ros/${ros_flavor}/setup.bash
+  export DISPLAY=${DISPLAY}
+fi"
+
+gsl_package="libgsl-dev"
+if [ "${flavor}" == "trusty" ]; then
+  gsl_package="libgsl0-dev"
+fi
+
+bunt_script="#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+sudo groupadd staff || true
+sudo groupadd messagebus || true
+
+# https://bugs.launchpad.net/ubuntu/+source/systemd/+bug/1325142/comments/38
+if [ \\\"${flavor}\\\" == \\\"trusty\\\" ]; then
+  sudo dpkg-divert --local --add /etc/init.d/systemd-logind
+  sudo ln -sf /bin/true /etc/init.d/systemd-logind
+fi
+
+# Needed on 18.04 to add key.
+sudo apt-get install gnupg -y || true
+
+grep \\\"${sources_add}\\\" /etc/apt/sources.list > /dev/null || sudo sed -i '\\\$s/\\\$/${sources_add}/' /etc/apt/sources.list
+
+sudo sh -c 'echo \\\"deb http://packages.ros.org/ros/ubuntu \\\$(lsb_release -sc) main\\\" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo apt-get update
+sudo apt-get install vim build-essential git -y
+sudo apt-get install ros-${ros_flavor}-ros-base ros-${ros_flavor}-tf2-ros -y
+sudo apt-get install python-catkin-tools python-wstool -y
+
+sudo rosdep init || true
+rosdep update
+
+grep \\\"${ros_flavor}\\\" ~/.bashrc || echo \\\"${bashrc_check}\\\" >> ~/.bashrc
+
+sudo apt-get install libarmadillo-dev libeigen3-dev libyaml-cpp-dev ${gsl_package} -y
+sudo apt-get install ros-${ros_flavor}-rviz ros-${ros_flavor}-xacro -y || true"
+
+schroot_conf="/etc/schroot/schroot.conf"
+
+# Only add the schroot entry if it's not already present.
+grep "\[${chroot_name}\]" "${schroot_conf}" > /dev/null || sudo sh -c "echo \"${schroot_conf_entry}\" >> ${schroot_conf}"
+
+script_filename="/setup_${flavor}_chroot.sh"
+
+# Copy Ubuntu install script into Ubuntu.
+sudo sh -c "echo \"${bunt_script}\" > \"${chroot_dir}/${script_filename}\""
+sudo chmod +x "${chroot_dir}/${script_filename}"
+
+# Ensure we have sudo access inside.
+sudo grep "${USER}" "${chroot_dir}/etc/sudoers" > /dev/null || sudo sh -c "echo \"${USER}	ALL=(ALL:ALL) ALL\" >> \"${chroot_dir}/etc/sudoers\""
+
+# Sometimes this file references unknown users leading to apt-get errors.
+sudo sh -c "sudo cat /dev/null > \"${chroot_dir}/var/lib/dpkg/statoverride\""
+
+echo "Going into the new chroot..."
+schroot --chroot=${chroot_name} "${script_filename}"
+echo "Enter the new Ubuntu with \"schroot -c ${chroot_name}\""
